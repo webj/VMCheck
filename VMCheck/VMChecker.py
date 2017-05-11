b044775db0182dd4dbbe25cfc3950cd507f5e500
Juerg Weber <ok.easy.man@gmx.net>
2017-05-11 12:04:51 +0200

    Extended testcases

--- VMCheck/VMChecker.py
+++ VMCheck/VMChecker.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import datetime
 import itertools
+from itertools import chain
 
 class VMChecker():
         
@@ -12,11 +13,12 @@ class VMChecker():
         
     def run(self):    
         self.printLog("\nStart VMCheck: %s" %datetime.datetime.now())
-        oldmonth = self.loadData('mock_maerz17.txt')        
+        oldmonth = self.loadData('mock_maerz17.txt')                
+        print(len(oldmonth))
         newmonth = self.loadData('mock_april17.txt')        
         if self.changes(oldmonth, newmonth):            
-            self.printLog("New VM: %s" %self.newVMs(oldmonth, newmonth))            
-            self.missingVMs(oldmonth, newmonth)            
+            self.newVMs(oldmonth, newmonth)          
+            self.missingLockedVMs(oldmonth, newmonth)      
             self.changingParameters(oldmonth, newmonth)                        
             self.createCSV(newmonth)
         else:
@@ -29,7 +31,7 @@ class VMChecker():
         mydict = {}       
         headerrow = next(csvreader, None)                       
         for row in csvreader:
-            key = row[3]            
+            key = row[headerrow.index('ID')]            
             if key in mydict:
                 if self.__debug:
                     self.printLog("Server %s is already in, check double value in csv." % key)
@@ -49,38 +51,47 @@ class VMChecker():
         newVMs = {}        
         for key in newmonth:
             if not key in oldmonth:
+               self.printLog("NEW VM: %s" %key)
                newVMs[key] = newmonth[key]
         return newVMs
     
-    #Checks if there are some VM' were deleted
-    def missingVMs(self, oldmonth, newmonth):        
+    #Checks if there were some VMs deleted. Adds to newmonth the locked VMs from previous month
+    def missingLockedVMs(self, oldmonth, newmonth):
+        missingVMs = {}        
         for key in oldmonth:
             if not key in newmonth:
                 if oldmonth[key]["Lock"]:
-                    self.printLog("Add deleted VM: %s, since it is still locked." %key)
+                    self.printLog("ADD deleted VM: %s, since it is still locked." %key)
                     newmonth[key] = oldmonth[key]
                 else:
-                    self.printLog("VM deleted (unlocked): %s" %key)
-                
+                    self.printLog("NOT ADDED: VM deleted (unlocked): %s" %key)
+
+    #checks differences in shared keys, needs two dicts of dicts.        
     def changingParameters(self, oldmonth, newmonth):        
-        for key in oldmonth:                        
+        sharedKeys = set(oldmonth.keys()).intersection(newmonth.keys())
+        for key in sharedKeys:                        
             if oldmonth[key]["Lock"] and not oldmonth[key] == newmonth[key]:                               
                 self.compare(oldmonth[key], newmonth[key])
-
+    
+    #Compares two dicts, to check wether two values are the same or not. If so, take the higher (if VM is still locked) otherwise take the newer.
+    #return updatet second dict
     def compare(self, first, second):        
         sharedKeys = set(first.keys()).intersection(second.keys())
         for key in sharedKeys:
             if first[key] != second[key]:            
                 self.printLog('Value Changed => Server: {}, Column: {}, Value Old: {}, Value New: {}'.format(first['ID'], key, first[key], second[key]))
-                if first[key] > second[key] and second['Lock']:
+                if first[key] > second[key]:
                     second[key] = first[key]
-                    self.printLog('Decreasment due lock not allowed: ID %s' %second['ID'])
-
+                    self.printLog('Move to previous month: ID %s' %second['ID'])
+        return second
+    
+    #Adds/Update dict entry lock date => key=Lock value=True/False
     def addLockDate(self, mydict):
         mydate = str(mydict['Commision Date'])[:10]
         mydatetime = datetime.datetime.strptime(mydate, '%Y-%m-%d')
         mydict['Lock'] =  mydatetime > (datetime.datetime.now() - datetime.timedelta(days=365))
     
+    #creates CSV for the new month
     def createCSV(self, datadict):
         with open('test.csv', 'w', newline='') as f:
             first= True
@@ -90,7 +101,8 @@ class VMChecker():
                     w.writerow(datadict[key].keys())
                     first = False
                 w.writerow(datadict[key].values())
-
+    
+    #Prints information into logfile
     def printLog(self, text):
         print(text, file=open('log.txt', 'a'))
 
